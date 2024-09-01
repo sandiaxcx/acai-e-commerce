e4986ae3ce721d901eb7e02192eb7e88d1bc5ef2
sandiaxcx <sandra8x@gmail.com>
2024-09-01 21:26:18 +0200

    create mixins

--- acai-e-commerce/src/views/dashboard.tsx
+++ acai-e-commerce/src/views/dashboard.tsx
@@ -2,22 +2,21 @@
 import Footer from "../components/layout/Footer";
 import { useState } from "react";
 import productData from "../data/products.json"
+import SalesChart from "../components/dashboard/SalesChart";
 
 const { products } = productData;
 
 export default function Dashboard() {
     const [selectedProductId, setSelectedProductId] = useState(products[0].id);
     const selectedProduct = products.find(product => product.id === selectedProductId);
-    
-    
+
     return (
         <section className="dashboardContainer">
-        <h1>Dashboard</h1>
-        {selectedProduct && (
-            <>
-
-            </>
-        )}
-        <Footer />
+            {selectedProduct && (
+                <section className="chartsContainer">
+                    <SalesChart product={selectedProduct} />    
+                </section>
+            )}
+            <Footer />
     </section>
     )};
