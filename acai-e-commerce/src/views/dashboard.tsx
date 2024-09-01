a11fee19601147feed061c7267cf06deee2de243
sandiaxcx <sandra8x@gmail.com>
2024-09-01 23:10:15 +0200

    create latest comments component

--- acai-e-commerce/src/views/dashboard.tsx
+++ acai-e-commerce/src/views/dashboard.tsx
@@ -5,13 +5,17 @@ import productData from "../data/products.json"
 import SalesChart from "../components/dashboard/SalesChart";
 import ConversionRateChart from "../components/dashboard/ConversionRateChart";
 import ReviewsChart from "../components/dashboard/ReviewsChart";
+import LatestComments from "../components/dashboard/LatestComments";
 
 const { products } = productData;
 
 export default function Dashboard() {
-    const [selectedProductId, setSelectedProductId] = useState(products[0].id);
+    const [selectedProductId, setSelectedProductId] = useState<number>(products[0]?.id || 1);
     const selectedProduct = products.find(product => product.id === selectedProductId);
 
+    if (!selectedProduct) {
+        return <div>No product found</div>;
+    }
     return (
         <section className="dashboardContainer">
             {selectedProduct && (
@@ -19,9 +23,13 @@ export default function Dashboard() {
                     <div className="midContainer">
                         <SalesChart product={selectedProduct} />    
                         <ConversionRateChart product={selectedProduct} />
-                        <ReviewsChart product={selectedProduct} />
+                    </div>                    
+                    <div className="botContainer">
+                        <ReviewsChart product={selectedProduct}/>
+                        <LatestComments product={selectedProduct} />
                     </div>
                 </section>
+                
             )}
             <Footer />
     </section>
