01c84f9a8a85b6278ec9d132ff5897fad1c26a93
sandiaxcx <sandra8x@gmail.com>
2024-09-01 23:39:46 +0200

    add styles to dashboard

--- acai-e-commerce/src/views/dashboard.tsx
+++ acai-e-commerce/src/views/dashboard.tsx
@@ -7,6 +7,7 @@ import ConversionRateChart from "../components/dashboard/ConversionRateChart";
 import ReviewsChart from "../components/dashboard/ReviewsChart";
 import LatestComments from "../components/dashboard/LatestComments";
 import ProductInventory from "../components/dashboard/ProductInventory";
+import "../styles/dashboard.scss";
 
 const { products } = productData;
 
@@ -14,11 +15,19 @@ export default function Dashboard() {
     const [selectedProductId, setSelectedProductId] = useState<number>(products[0]?.id || 1);
     const selectedProduct = products.find(product => product.id === selectedProductId);
 
-    if (!selectedProduct) {
-        return <div>No product found</div>;
-    }
     return (
         <section className="dashboardContainer">
+                        <div className="selectorContainer">
+                <select 
+                value={selectedProductId}
+                onChange={(e) => setSelectedProductId(Number(e.target.value))}>
+                {products.map(product => (
+                    <option key={product.id} value={product.id}>
+                        {product.name}
+                    </option>
+                ))}
+                </select>
+            </div>
             {selectedProduct && (
                 <section className="chartsContainer">
                     <div className="topContainer">
