409543e8de421865b60b729920bdc1264280fa25
sandiaxcx <sandra8x@gmail.com>
2024-09-01 20:56:04 +0200

    import products database to dashboard

--- acai-e-commerce/src/views/dashboard.tsx
+++ acai-e-commerce/src/views/dashboard.tsx
@@ -1,9 +1,23 @@
+
 import Footer from "../components/layout/Footer";
+import { useState } from "react";
+import productData from "../data/products.json"
+
+const { products } = productData;
 
 export default function Dashboard() {
+    const [selectedProductId, setSelectedProductId] = useState(products[0].id);
+    const selectedProduct = products.find(product => product.id === selectedProductId);
+    
+    
     return (
         <section className="dashboardContainer">
-            <h1>Dashboard</h1>
-            <Footer />
-        </section>
+        <h1>Dashboard</h1>
+        {selectedProduct && (
+            <>
+
+            </>
+        )}
+        <Footer />
+    </section>
     )};
