14ed97c61c15a4ceb3d5fc202c2c0bd1e92c82b6
sandiaxcx <sandra8x@gmail.com>
2024-09-01 19:34:06 +0200

    clean up app.tsx, import header, dashboard, footer

---
+++ acai-e-commerce/src/components/layout/Footer.tsx
@@ -0,0 +1,6 @@
+export default function Footer() {
+    return (
+        <footer>
+            <h1>Footer</h1>
+        </footer>
+    )};
---
+++ acai-e-commerce/src/views/dashboard.tsx
@@ -0,0 +1,9 @@
+import Footer from "../components/layout/Footer";
+
+export default function Dashboard() {
+    return (
+        <section className="dashboardContainer">
+            <h1>Dashboard</h1>
+            <Footer />
+        </section>
+    )};
